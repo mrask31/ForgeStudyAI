ebe6f6af4e5b78486d02f3735efcecaafb63d6fc
mrask31 <admin@mjrintelligencegroup.com>
2026-03-06 12:05:08 -0600

    fix: Update Stripe API version to 2026-02-25.clover

--- src/app/actions/student-profiles.ts
+++ src/app/actions/student-profiles.ts
@@ -34,7 +34,7 @@ const getStripeClient = () => {
     throw new Error('Stripe is not configured')
   }
   return new Stripe(process.env.STRIPE_SECRET_KEY, {
-    apiVersion: '2025-12-15.clover',
+    apiVersion: '2026-02-25.clover',
   })
 }
 
